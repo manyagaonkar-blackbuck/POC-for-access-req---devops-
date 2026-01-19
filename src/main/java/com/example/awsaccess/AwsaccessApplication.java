9b720d4a00ed66b477a9aebe59337e4d391851f8
Manya Chandrashekar <manyachandrashekar@Manya.local>
2026-01-19 15:37:16 +0530

    Initial Spring Boot setup using Gradle Groovy

---
+++ src/main/java/com/example/awsaccess/AwsaccessApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsaccessApplication.class, args);
+	}
+
+}
